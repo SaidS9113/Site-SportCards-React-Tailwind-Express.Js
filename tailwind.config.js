5d24e4da3ac6fbbef46d97ec3e9a882a39970cd6
said <soidroudinesaid@hotmail.fr>
2024-11-17 20:53:43 +0100

    Création de l'environnement React et Tailwind en commançant par le Header

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ], // Modifiez ce chemin selon la structure de votre projet
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
